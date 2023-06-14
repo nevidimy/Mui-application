4d29dbde3bb2cd8769f3b9b639c7dfed5583bbcb
Yevgen <evgennevidim@gmail.com>
2023-06-14 16:37:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import NavDrawer from "./Components/Scaffold/NavDrawer";
+
+function App() {
+    return (
+        <NavDrawer/>
+    );
+}
+
+export default App;
